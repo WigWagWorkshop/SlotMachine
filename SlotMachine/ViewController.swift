b4cee315b159303a9e3220d8cd704560501ed77d
Steven Pezzi <steven@WigWagWorkshop.local>
2014-11-25 18:50:01 -0500

    Initial Commit

---
+++ SlotMachine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlotMachine
+//
+//  Created by Steven Pezzi on 11/25/14.
+//  Copyright (c) 2014 The Pez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
